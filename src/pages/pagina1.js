aa658987ea92215dcf6c24c5c232cf8ae2569f91
armeniocardoso <armeniotorres@gmail.com>
2023-10-26 14:31:05 -0300

    Commit Inicial

---
+++ src/pages/pagina1.js
@@ -0,0 +1,36 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h2>Entre em Contato:</h2>
+      <div className="container">
+        <form name="form_estatico" method="post"  data-netlify="true">
+          <label>
+            Nome
+            <input type="text" name="nome" />
+          </label>
+          <label>
+            Email
+            <input type="email" name="email" />
+          </label>
+          <label>
+            Assunto
+            <input type="text" name="assunto" />
+          </label>
+          <label>
+            Mensagem
+            <textarea name="mensagem" rows="5" />
+          </label>
+          <input type="submit" value="Enviar" />
+          <input type="reset" value="Limpar" />
+        </form>
+      </div>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const Head = () => <title>Início</title>
